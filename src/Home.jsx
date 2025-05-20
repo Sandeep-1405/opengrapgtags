9c8ff6eae206e8bd13e7b7ba037fc9f998e873d0
Sandeep <sandeepmaraboina14s@gmail.com>
2025-05-20 11:22:00 +0530

    master

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,28 +7,7 @@ const Home = () => {
     <div>
       <Helmet>
         <title>Home</title>
-        <meta property="og:title" content="Your Website Title" />
-        <meta
-          property="og:description"
-          content="A short description of your website or page."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://yourwebsite.com" />
-        <meta
-          property="og:image"
-          content="https://wordtracker-swoop-uploads.s3.amazonaws.com/uploads/ckeditor/pictures/2090/content_beach.jpg"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Your Website Title" />
-        <meta
-          name="twitter:description"
-          content="A short description of your website or page."
-        />
-        <meta
-          name="twitter:image"
-          content="https://wordtracker-swoop-uploads.s3.amazonaws.com/uploads/ckeditor/pictures/2090/content_beach.jpg"
-        />
+        
       </Helmet>
       <h1>Home 1</h1>
       <Link to="/settings">Go to settings</Link>
