ead0d237431effedd5df1e4fe969687805d3bd7a
Sandeep <sandeepmaraboina14s@gmail.com>
2025-05-20 11:18:05 +0530

    new

---
+++ src/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings</h1>
+            <Link to="/">Go to home</Link>
+        </div>
+    );
+};
+
+export default Settings;
